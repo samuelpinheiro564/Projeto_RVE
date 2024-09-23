4a72869baff48ebb5ed5232f0c427a2b24f4d23b
Victor Hugo Mattos Maria <victormaria@aluno.senai.br>
2024-09-23 15:46:50 -0300

    refactor: Atualizar rotas de manipulação de atestados

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,33 @@
 require('dotenv').config();
 const express = require('express');
-const bruxosRoutes = require('./routes/bruxosRoutes');
-const varinhasRoutes = require('./routes/varinhasRoutes');
+const atestadoRoutes = require("../src/routes/atestado.routes")
+const CampoTextoRoutes = require("../src/routes/CampoTexto.routes")
+const saidaRoutes = require("../src/routes/saida.routes")
+const usuariosRoutes = require("../src/routes/usuarios.routes")
+const RveRoutes = require("../src/routes/rve.routes")
+const ForumRoutes = require("../src/routes/forum.routes")
+
 
 const app = express();
-const port = process.env.PORT || 3000;  // Default to 3000 if PORT is not set
+const port = process.env.PORT || 3000; 
 
 app.use(express.json());
 
-app.use('/', bruxosRoutes);
-app.use('/', varinhasRoutes);
+app.use(atestadoRoutes);
+app.use(CampoTextoRoutes);
+app.use(saidaRoutes);
+app.use(usuariosRoutes);
+app.use(RveRoutes);
+app.use(ForumRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API do projeto de atestados");
+}
+);
+
+
+
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}🧙🪄✨`);
+  console.log(`Servidor rodando na porta ${port}`);
 });
