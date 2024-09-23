b0aafa8b1956cd37fdc74fbd322ab8412ad2dc98
samuelpinheiro564 <samuel.pinheiro2@aluno.senai.br>
2024-09-23 08:30:42 -0300

    feat: Adicionar tabelas Saida, Usuario, Atestado, RVE, Forum e CampoTexto
    Adiciona as tabelas Saida, Usuario, Atestado, RVE, Forum e CampoTexto ao arquivo db/tables.sql.

---
+++ db/tables.sql
@@ -0,0 +1,73 @@
+CREATE TABLE Saida(
+id INT PRIMARY KEY AUTO_INCREMENT,
+NomeALuno VARCHAR(255),
+Curso VARCHAR(255),
+DataSaida DATE,
+HoraSaida TIME,
+Turma VARCHAR(255),
+AlunoRA INT,
+MaiorIdade BOOLEAN,
+LiberadoSec BOOLEAN,
+AssinaturaAnaq BOOLEAN,
+AssinaturaProf BOOLEAN,
+);
+
+CREATE TABLE Usuario(
+    Nif INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(255),
+    Email VARCHAR(255),
+    Senha VARCHAR(255),
+    Telefone INT,
+    Tipo VARCHAR(255),
+);
+
+CREATE TABLE Atestado(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    NomeAluno VARCHAR(255),
+    Curso VARCHAR(255),
+    Turma VARCHAR(255),
+    ImagemAtestado TEXT,
+    Aluno RA INT,
+    CID VARCHAR(255),
+    DataInicio DATE,
+    DataFim DATE,
+    AssinaturaAnaq BOOLEAN,
+    AssinaturaProf1 BOOLEAN,
+    AssinaturaProf2 BOOLEAN,
+    AssinaturaProf3 BOOLEAN,
+    AssinaturaProf4 BOOLEAN,
+);
+
+CREATE TABLE RVE(
+ Id INT PRIMARY KEY AUTO_INCREMENT,
+ Autor VARCHAR(255),
+ Estudante VARCHAR(255),
+ Curso VARCHAR(255),
+ Turma VARCHAR(255),
+ Data DATE,
+ Hora TIME,
+ Motivo VARCHAR(255),
+ OrientaçõesEstudante TEXT,
+ DescricaoOcorrido TEXT,
+ DocentesEnvolvidos TEXT,
+ Assinaturas Text,
+ Elogios TEXT,
+ Dificuldades TEXT,
+ Presença TEXT,
+);
+
+CREATE TABLE Forum(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    IdRVE INT,
+    IDCampoTexto INT,
+   FOREGIN KEY (IdRVE) REFERENCES RVE(Id),
+   FOREGIN KEY (IDCampoTexto) REFERENCES CampoTexto(ID),
+);
+
+CREATE TABLE CampoTexto(
+    Id INT PRIMARY KEY AUTO_INCREMENT,
+    NifTextoDocente INT,
+    Texto TEXT,
+    Assinado BOOLEAN,
+    FOREGIN KEY (NifTextoDocente) REFERENCES Usuario(Nif),
+);
