ab85b758b766e9cab20c7bc9838dac8821a9193d
Victor Hugo Mattos Maria <victormaria@aluno.senai.br>
2024-09-23 15:47:07 -0300

    refactor: Adicionar CRUD completo para rotas de CampoTexto

--- src/routes/CampoTexto.routes.js
+++ src/routes/CampoTexto.routes.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const CampoTextoController = require("../Controller/CampoTexto")
 
+router.post("/camposTexto", CampoTextoController.createCampoTexto);
+router.get("/camposTexto", CampoTextoController.getCamposTexto);
+router.get("/camposTexto/:id", CampoTextoController.getCampoTextoById);
+router.put("/camposTexto/:id", CampoTextoController.updateCampoTexto);
+router.delete("/camposTexto/:id", CampoTextoController.deleteCampoTexto);
+
+module.exports = router;
