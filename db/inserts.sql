2fad860f49a9c9389d5bedf872222a0e644e0b6b
samuelpinheiro564 <samuel.pinheiro2@aluno.senai.br>
2024-09-23 08:38:15 -0300

    feat: Adicionar inserção de usuários no arquivo db/inserts.sql
    Adiciona os registros de usuários no arquivo db/inserts.sql.

---
+++ db/inserts.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Usuarios` (`Nome`,`Email`,`Senha`,`Telefone`,`Tipo`) VALUES
+('John Doe','123456',12345678,'Admin'),
+('Jane Doe','123456',12345678,'Admin'),
+('João Silva','123456',12345678,'Admin'),
+('Maria Silva','123456',12345678,'Admin');
