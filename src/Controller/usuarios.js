030e2b7f11f73f080de8d8220b6fd162cc1644a4
Samuel Pinheiro <samuel.pinheiro2@aluno.senai.br>
2024-10-07 15:37:24 -0300

    aaaaa

--- src/Controller/usuarios.js
+++ src/Controller/usuarios.js
@@ -1,38 +1,44 @@
 const pool = require('../config/dbConfig');
 
 async function AllUser(req, res)  {
+try {
     const { rows } = await pool.query("SELECT * FROM Usuarios");
     res.json(rows);
+}
+catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+}
 };
 
 async function CreateUser (req, res)  {
-    const { Nif,Nome, Email, Senha, Telefone, Tipo } = req.body;
+    const { nif,nome,email,senha,telefone,tipo } = req.body;
     await pool.query(
-        "INSERT INTO Usuarios (Nif,Nome, Email, Senha, Telefone, Tipo) VALUES ($1, $2, $3, $4, $5,$6)",
-        [Nif,Nome, Email, Senha, Telefone, Tipo]
+        "INSERT INTO Usuarios ( nif,nome,email,senha,telefone,tipo) VALUES ($1, $2, $3, $4, $5,$6)",
+        [nif,nome,email,senha,telefone,tipo]
     );
     res.json({ message: "Usuario criado" });
 };
 
  async function AtualizaUser (req, res)  {
-    const { Nif } = req.params;
-    const { Nome, Email, Senha, Telefone, Tipo } = req.body;
+    const { nif } = req.params;
+    const { nome, email, senha, telefone, tipo } = req.body;
     await pool.query(
-        "UPDATE Usuarios SET Nome = $1, Email = $2, Senha = $3, Telefone = $4, Tipo = $5 WHERE id = $6",
-        [Nome, Email, Senha, Telefone, Tipo, Nif]
+        "UPDATE Usuarios SET nome = $1, email = $2, senha = $3, telefone = $4, tipo = $5 WHERE nif = $6",
+        [nome, email, senha, telefone, tipo, nif]
     );
     res.json({ message: "Usuario atualizado" });
 };
 
 async function DeleteUSer (req, res)  {
-    const { Nif } = req.params;
-    await pool.query("DELETE FROM Usuarios WHERE Nif = $1", [Nif]);
+    const { nif } = req.params;
+    await pool.query("DELETE FROM Usuarios WHERE nif = $1", [nif]);
     res.json({ message: "Usuario deletado" });
 };
 async function Login (req, res)  {
     try {
-        const { Nif } = req.params;
-        const { rows } = await pool.query("SELECT * FROM Usuarios WHERE Nif = $1", [Nif]);
+        const { nif } = req.params;
+        const { rows } = await pool.query("SELECT * FROM Usuarios WHERE nif = $1", [nif]);
         res.json(rows);
     } catch (error) {
         console.error(error);
