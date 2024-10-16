96a5ed532fc53af1603861bca62f691ca4b25eeb
Victor Mattos <victormaria@aluno.senai.br>
2024-10-16 11:37:39 -0300

    Refatorar rota de exclusão de registro de saída

--- src/routes/saidaRoutes.js
+++ src/routes/saidaRoutes.js
@@ -7,6 +7,6 @@ router.post("/saida", SaidaController.createSaidaRecord);
 router.get("/saida", SaidaController.getSaidaRecords);
 router.get("/saida/:id", SaidaController.getSaidaRecordById);
 router.put("/saida/:id", SaidaController.updateSaidaRecord);
-
+router.delete("/saida/:id", SaidaController.deleteSaidaRecord);
 
 module.exports = router;
