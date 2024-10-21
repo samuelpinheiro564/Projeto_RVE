e383bcc2e4f8561f48e96bb517f716b241974110
Victor Mattos <victormaria@aluno.senai.br>
2024-10-21 11:48:39 -0300

    Refatorar importações de rotas em src/index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ const atestadosRoutes = require("../src/routes/atestadosRoutes")
 const CampoTextoRoutes = require("../src/routes/campoTextoRoutes")
 const ForumRoutes = require("../src/routes/forumRoutes")
 const SaidaRoutes = require("../src/routes/saidaRoutes")
+const SaidaProfessor = require("../src/routes/saidaProfessorRoutes")
 const usuarios = require("../src/routes/usuarios.routes")
+
 const RveRoutes = require("../src/routes/rveRoutes")
 const cors = require('cors');  
  
@@ -20,6 +22,7 @@ app.use(atestadosRoutes);
 app.use(CampoTextoRoutes);
 app.use(ForumRoutes);
 app.use(SaidaRoutes);
+app.use(SaidaProfessor);
 app.use(usuarios);
 app.use(RveRoutes);
 
