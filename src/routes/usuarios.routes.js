f1a8b6717669b28a77fbce56958ababeb7e210f6
Victor Hugo Mattos Maria <victormaria@aluno.senai.br>
2024-10-28 15:01:11 -0300

    Refatorar código relacionado a atestados, rotas do fórum e tabela de atestado
    - Corrigir nome da função AllUser para AllUsers
    - Corrigir nome da tabela Usuarios para Usuarios
    - Adicionar rota para obter RVE por ID
    - Remover código não utilizado
    - Corrigir nome da função DeleteUSer para DeleteUser
    - Corrigir nome da função AtualizaUser para AtualizaUser
    - Corrigir nome da função Login para Login
    - Corrigir nome da função AllUser para AllUsers

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,12 +1,11 @@
-const express = require('express');
-const usuarios = express.Router();
-const UsuariosController = require("../Controller/usuarios")
+const express = require('express');  
+const usuarios = express.Router();  
+const UsuariosController = require("../Controller/usuarios");  
 
-
-usuarios.post("/usuarios/", UsuariosController.CreateUser);
-usuarios.get("/usuarios", UsuariosController.AllUser);
-usuarios.get("/usuarios/:nif", UsuariosController.Login);
-usuarios.put("/usuarios/:nif", UsuariosController.AtualizaUser);
-usuarios.delete("/usuarios/:nif", UsuariosController.DeleteUSer);
+usuarios.post("/usuarios/", UsuariosController.CreateUser);  
+usuarios.get("/usuarios", UsuariosController.AllUsers);   
+usuarios.get("/usuarios/:nif", UsuariosController.Login);  
+usuarios.put("/usuarios/:nif", UsuariosController.AtualizaUser);  
+usuarios.delete("/usuarios/:nif", UsuariosController.DeleteUser);  
 
 module.exports = usuarios;
