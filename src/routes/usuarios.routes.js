fe3d18bb5b1de5db0c56a5e979aa5781cee50ffb
samuelpinheiro564 <samuel.pinheiro2@aluno.senai.br>
2024-11-28 15:41:40 -0300

    Atualizar rotas de usuários para corrigir endpoint de criação e adicionar busca por NIF

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const usuarios = express.Router();  
 const UsuariosController = require("../Controller/usuarios");  
 
-usuarios.post("/usuarios/", UsuariosController.CreateUser);  
-usuarios.get("/usuarios", UsuariosController.AllUsers);  
+usuarios.post("/usuarios", UsuariosController.CreateUser);  
+usuarios.get("/usuarios/:nif", UsuariosController.AllUsers);  
 usuarios.get("/usuarios/nome/:nome", UsuariosController.UserName);   
 usuarios.get("/usuarios/:nif/:senha", UsuariosController.Login);  
 usuarios.put("/usuarios/:nif", UsuariosController.AtualizaUser);  
