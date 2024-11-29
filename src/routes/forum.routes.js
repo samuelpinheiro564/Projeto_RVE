4d1917a2ce74387e76195dc3510d4a8ad8351ef1
samuelpinheiro564 <samuel.pinheiro2@aluno.senai.br>
2024-11-28 23:10:45 -0300

    Remover linhas em branco desnecessárias no arquivo de rotas do fórum

--- src/routes/forum.routes.js
+++ src/routes/forum.routes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const ForumController = require("../Controller/Forum")
 
-
 router.post("/rve_usuarios", ForumController.CreateRve);
 router.get("/rve_usuarios/:usuario_nif", ForumController.getAllRveByNif);
-
 
 module.exports = router;
